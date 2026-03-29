d64893f503b6849a50b23fc14438f91d5baa0b1b
Prajwin KJ <prajwinkj1232@gmail.com>
2026-03-29 21:57:56 +0530

    ready for receiving data in the forms table

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
+from sqlalchemy import create_engine
+from sqlalchemy.orm import DeclarativeBase,Mapped,mapped_column,Session
 
 app=FastAPI()
 
@@ -16,7 +18,23 @@ class Form_structure(BaseModel):
     title: str
     description: str
 
+class Base(DeclarativeBase):
+    pass
+
+engine=create_engine("postgresql+psycopg2://minimalform:password@localhost:5432/form")
+
+class Form(Base):
+    __tablename__="forms"
+    id:Mapped[int] =mapped_column(primary_key=True)
+    title:Mapped[str] =mapped_column(nullable=False)
+    description:Mapped[str] =mapped_column(nullable=True)
+
+Base.metadata.create_all(engine)
+
 @app.post("/api/forms")
 def form(data: Form_structure):
-    print(data.title,data.description)
-    return "ok"
+    Form1=Form(title=data.title,description=data.description)
+    with Session(engine) as session:
+        session.add(Form1)
+        session.commit()
+    return "Success"
