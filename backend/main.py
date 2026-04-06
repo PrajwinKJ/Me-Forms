ca49ff41bb7f41b974700fd8667e0a27dcb05b7a
Prajwin KJ <prajwinkj1232@gmail.com>
2026-04-07 00:55:38 +0530

    created userlogin aand signup

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,40 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Depends,HTTPException
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy import create_engine,ForeignKey
 from sqlalchemy.orm import DeclarativeBase,Mapped,mapped_column,Session
 from sqlalchemy import JSON
 import os
+from passlib.context import CryptContext
+from jose import jwt
+from datetime import datetime,timedelta,UTC
+
+pwdcontext=CryptContext(schemes=["sha256_crypt"],deprecated="auto")
+def hashpwd(password):
+    return pwdcontext.hash(password)
+
+def verifypwd(password,hashed):
+    return pwdcontext.verify(password,hashed)
+
+def create_token(userid):
+    Secret_key=str(os.getenv("SecretKey"))
+    alg="HS256"
+    payload={
+        "userid":userid,
+        "exp":datetime.now(UTC) + timedelta(hours=3)
+    }
+    token=jwt.encode(payload,Secret_key,alg)
+    return token
+security=HTTPBearer()
+def gcu(credentials: HTTPAuthorizationCredentials=Depends(security)):
+    token=credentials.credentials
+    key=str(os.getenv("SecretKey"))
+    try:
+        payload=jwt.decode(token,key,algorithms="HS256")
+        return payload.get("userid")
+    except:
+        raise HTTPException(status_code=401,detail="Invalid token")
 
 app=FastAPI()
 
@@ -17,6 +47,10 @@ app.add_middleware(
 )
 
 from typing import List
+
+class User_struct(BaseModel):
+    email: str
+    password: str
 
 class Question_structure(BaseModel):
     id: str  
@@ -41,15 +75,22 @@ class Response_structure(BaseModel):
 class Base(DeclarativeBase):
     pass
 
-dburl=os.getenv("DatabaseUrl")
+dburl=str(os.getenv("DatabaseUrl"))
 if dburl.startswith("postgres://"):
     dburl=dburl.replace("postgres://","postgresql://")
 
 engine=create_engine(dburl)
 
+class User(Base):
+    __tablename__="users"
+    id:Mapped[int]=mapped_column(primary_key=True)
+    email:Mapped[str]=mapped_column(nullable=False,unique=True)
+    password:Mapped[str]=mapped_column(nullable=False)
+
 class Form(Base):
     __tablename__="forms"
     id:Mapped[int] =mapped_column(primary_key=True)
+    user_id:Mapped[int] =mapped_column(ForeignKey("users.id"))
     title:Mapped[str] =mapped_column(nullable=False)
     description:Mapped[str] =mapped_column(nullable=True)
 
@@ -77,9 +118,9 @@ def root():
     }
 
 @app.post("/api/forms")
-def form(data: Form_structure):
+def form(data: Form_structure,userid: int=Depends(gcu)):                                             
     with Session(engine) as session:
-        Formobj=Form(title=data.title,description=data.description)
+        Formobj=Form(title=data.title,description=data.description,user_id=userid)
         session.add(Formobj)
         session.flush()
         for q in data.questions:
@@ -92,15 +133,15 @@ def form(data: Form_structure):
     return form_id
 
 @app.get("/api/forms")
-def get_all_forms():
+def get_all_forms(userid: int=Depends(gcu)):
     with Session(engine) as session:
-        forms = session.query(Form).all()
+        forms = session.query(Form).filter(Form.user_id==userid).all()
         return [{"id": form.id, "title": form.title, "description": form.description} for form in forms]
 
 @app.get("/api/forms/{formid}")
-def getform(formid: int):
+def getform(formid: int,userid: int=Depends(gcu)):
     with Session(engine) as session:
-        form=session.get(Form,formid)
+        form=session.query(Form).filter(Form.id==formid,Form.user_id==userid).first()
         questions=session.query(Question).filter(Question.form_id==formid).all()
     if form:
         return{
@@ -119,9 +160,9 @@ def getform(formid: int):
         }
     
 @app.get("/api/forms/{formid}/responses")
-def response(formid: int):
+def response(formid: int, userid: int =Depends(gcu)):
     with Session(engine) as session:
-        form=session.get(Form,formid)
+        form=session.query(Form).filter(Form.id==formid,Form.user_id==userid).first()
         questions=session.query(Question).filter(Question.form_id==formid).all()
         answers=session.query(Response).filter(Response.form_id==formid).all()
     
@@ -153,3 +194,38 @@ def submit_response(formid: int, data: Response_structure):
             session.add(answer)
         session.commit()
     return {"message": "Success"}
+
+@app.post("/api/signup")
+def signup(data: User_struct):
+    with Session(engine) as ss:
+        exist=ss.query(User).filter(User.email== data.email).first()
+        if exist:
+            return{
+                "error":"User already exists"
+            }
+        pwd=hashpwd(data.password)
+        user=User(email=data.email,password=pwd)
+
+        ss.add(user)
+        ss.commit()
+    
+    return {
+        "Success":"User created successfully"
+    }
+
+@app.post("/api/login")
+def login(data: User_struct):
+    with Session(engine) as ss:
+        user=ss.query(User).filter(User.email==data.email).first()
+        if not user:
+            return{
+                "Error":"User doesn't exist"
+            }
+        if not verifypwd(data.password,user.password):
+            return {
+                "Error":"Invalid credentials"
+            }
+        token=create_token(user.id)
+    return {
+        "access_token":token
+    }
